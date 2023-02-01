ebdf78c444a7f15cc33640c2e29e543b3de4f458
Vojtěch Bartička <aitakaitov@gmail.com>
2023-02-01 11:00:50 +0100

    SST attributions generation changes
    removed times, added distinction between certain and uncertain predictions

--- create_attributions_sst.py
+++ create_attributions_sst.py
@@ -3,14 +3,14 @@ import json
 from transformers import AutoTokenizer
 from attribution_methods_custom import gradient_attributions, ig_attributions, sg_attributions
 from models.bert_512 import BertSequenceClassifierSST
-import numpy as np
 from BERT_explainability.modules.BERT.ExplanationGenerator import Generator
 
-from time import perf_counter_ns
 import os
 import argparse
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+UNSURE_PREDICTION_DELTA = 0.1
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--output_dir', default='output_sst_attrs', help='Attributions output directory')
@@ -23,8 +23,13 @@ OUTPUT_DIR = args.output_dir
 MODEL_PATH = args.model_path
 BASELINES_DIR = args.baselines_dir
 
+CERTAIN_DIR = 'certain'
+UNSURE_DIR = 'unsure'
+
 try:
     os.mkdir(OUTPUT_DIR)
+    os.mkdir(os.path.join(OUTPUT_DIR, CERTAIN_DIR))
+    os.mkdir(os.path.join(OUTPUT_DIR, UNSURE_DIR))
 except OSError:
     pass
 
@@ -118,106 +123,84 @@ def create_neutral_baseline(sentence):
 
 #   -----------------------------------------------------------------------------------------------
 
-def create_gradient_attributions(sentences, target_indices):
+def create_gradient_attributions(sentences, target_indices, target_dir=CERTAIN_DIR):
     attrs = []
-    times = []
     for sentence, target_idx in zip(sentences, target_indices):
         input_embeds, attention_mask = prepare_embeds_and_att_mask(sentence)
-        start_time = perf_counter_ns()
         attr = gradient_attributions(input_embeds, attention_mask, target_idx, model)
-        times.append(perf_counter_ns() - start_time)
         attr = torch.squeeze(attr)
         attrs.append(format_attrs(attr, sentence))
 
-    with open(OUTPUT_DIR + '/' + method_file_dict['grads'], 'w+', encoding='utf-8') as f:
-        f.write(json.dumps([attrs, float(np.average(times))]))
+    with open(str(os.path.join(OUTPUT_DIR, target_dir, method_file_dict['grads'])), 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(attrs))
 
     attrs = []
-    times = []
     for sentence, target_idx in zip(sentences, target_indices):
         input_embeds, attention_mask = prepare_embeds_and_att_mask(sentence)
-        start_time = perf_counter_ns()
         attr = gradient_attributions(input_embeds, attention_mask, target_idx, model, True)
-        times.append(perf_counter_ns() - start_time)
         attr = torch.squeeze(attr)
         attrs.append(format_attrs(attr, sentence))
 
-    with open(OUTPUT_DIR + '/' + method_file_dict['grads_x_inputs'], 'w+', encoding='utf-8') as f:
-        f.write(json.dumps([attrs, float(np.average(times))]))
+    with open(str(os.path.join(OUTPUT_DIR, target_dir, method_file_dict['grads_x_inputs'])), 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(attrs))
 
 
-def _do_ig(sentences, target_indices, steps, file):
+def _do_ig(sentences, target_indices, steps, file, target_dir):
     attrs = []
-    times = []
     for sentence, target_idx in zip(sentences, target_indices):
         input_embeds, attention_mask = prepare_embeds_and_att_mask(sentence)
-        start_time = perf_counter_ns()
         baseline = create_neutral_baseline(sentence)
         attr = ig_attributions(input_embeds, attention_mask, target_idx, baseline, model, steps)
-
-        times.append(perf_counter_ns() - start_time)
         attr = torch.squeeze(attr)
         attrs.append(format_attrs(attr, sentence))
 
-    with open(method_file_dict[file], 'w+', encoding='utf-8') as f:
-        f.write(json.dumps([attrs, float(np.average(times))]))
+    with open(str(os.path.join(OUTPUT_DIR, target_dir, method_file_dict[file])), 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(attrs))
 
 
-def create_ig_attributions(sentences, target_indices):
-    _do_ig(sentences, target_indices, 20, 'ig_20')
-    _do_ig(sentences, target_indices, 50, 'ig_50')
-    _do_ig(sentences, target_indices, 100, 'ig_100')
-    _do_ig(sentences, target_indices, 200, 'ig_200')
+def create_ig_attributions(sentences, target_indices, target_dir=CERTAIN_DIR):
+    _do_ig(sentences, target_indices, 20, 'ig_20', target_dir)
+    _do_ig(sentences, target_indices, 50, 'ig_50', target_dir)
+    _do_ig(sentences, target_indices, 100, 'ig_100', target_dir)
+    _do_ig(sentences, target_indices, 200, 'ig_200', target_dir)
 
 
-def _do_sg(sentences, target_indices, samples, file):
+def _do_sg(sentences, target_indices, samples, file, target_dir):
     attrs = []
     attrs_x_inputs = []
-    times = []
-    times_x_input = []
     for sentence, target_idx in zip(sentences, target_indices):
         input_embeds, attention_mask = prepare_embeds_and_att_mask(sentence)
-        start_time_1 = perf_counter_ns()
         attr = sg_attributions(input_embeds, attention_mask, target_idx, model, samples, 0.2)
-        end_time_1 = perf_counter_ns()
-        start_time_2 = perf_counter_ns()
         attr_x_input = attr.to(device) * input_embeds
-        end_time_2 = perf_counter_ns()
         attr_x_input = torch.squeeze(attr_x_input)
         attr = torch.squeeze(attr)
         attrs.append(format_attrs(attr, sentence))
         attrs_x_inputs.append(format_attrs(attr_x_input, sentence))
 
-        times.append(end_time_1 - start_time_1)
-        times_x_input.append((end_time_1 - start_time_1) + (end_time_2 - start_time_2))
+    with open(str(os.path.join(OUTPUT_DIR, target_dir, method_file_dict[file])), 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(attrs))
 
-    with open(method_file_dict[file], 'w+', encoding='utf-8') as f:
-        f.write(json.dumps([attrs, float(np.average(times))]))
-
-    with open(method_file_dict[file + '_x_inputs'], 'w+', encoding='utf-8') as f:
-        f.write(json.dumps([attrs_x_inputs, float(np.average(times_x_input))]))
+    with open(str(os.path.join(OUTPUT_DIR, target_dir, method_file_dict[file + '_x_inputs'])), 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(attrs_x_inputs))
 
 
-def create_smoothgrad_attributions(sentences, target_indices):
-    _do_sg(sentences, target_indices, 20, 'sg_20')
-    _do_sg(sentences, target_indices, 50, 'sg_50')
-    _do_sg(sentences, target_indices, 100, 'sg_100')
-    _do_sg(sentences, target_indices, 200, 'sg_200')
+def create_smoothgrad_attributions(sentences, target_indices, target_dir=CERTAIN_DIR):
+    _do_sg(sentences, target_indices, 20, 'sg_20', target_dir)
+    _do_sg(sentences, target_indices, 50, 'sg_50', target_dir)
+    _do_sg(sentences, target_indices, 100, 'sg_100', target_dir)
+    _do_sg(sentences, target_indices, 200, 'sg_200', target_dir)
 
 
-def create_relprop_attributions(sentences, target_indices):
+def create_relprop_attributions(sentences, target_indices, target_dir=CERTAIN_DIR):
     attrs = []
-    times = []
     for sentence, target_idx in zip(sentences, target_indices):
         input_ids, attention_mask = prepare_input_ids_and_attention_mask(sentence)
         inputs_embeds, _ = prepare_embeds_and_att_mask(sentence)
-        start_time = perf_counter_ns()
         res = relprop_explainer.generate_LRP(input_ids=input_ids, attention_mask=attention_mask, start_layer=0, index=target_idx)
-        times.append(perf_counter_ns() - start_time)
         attrs.append(format_attrs(res, sentence))
 
-    with open(OUTPUT_DIR + '/' + method_file_dict['relprop'], 'w+', encoding='utf-8') as f:
-        f.write(json.dumps([attrs, float(np.average(times))]))
+    with open(str(os.path.join(OUTPUT_DIR, target_dir, method_file_dict['relprop'])), 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(attrs))
 
 
 #   -----------------------------------------------------------------------------------------------
@@ -226,11 +209,18 @@ def create_relprop_attributions(sentences, target_indices):
 def main():
     sentences, tokens, phrase_sentiments = get_sentences_tokens_and_phrase_sentiments()
 
-    # first extract the general information - sst_tokens, bert_tokens
-    bert_tokens = []
-    sst_tokens = []
-    target_indices = []
-    valid_sentences = []
+    # for correct and sure predictions
+    bert_tokens_correct = []
+    sst_tokens_correct = []
+    correct_pred_indices = []
+    correct_pred_sentences = []
+
+    # for correct but unsure predictions
+    bert_tokens_unsure = []
+    sst_tokens_unsure = []
+    unsure_pred_indices = []
+    unsure_pred_sentences = []
+
     for sentence, tokens in zip(sentences, tokens):
         # create_neutral_baseline(sentence)
         # first classify the sample
@@ -238,27 +228,40 @@ def main():
         res = model(inputs_embeds=input_embeds, attention_mask=attention_mask, return_logits=False, inputs_embeds_in_input_ids=False)
         top_idx = int(torch.argmax(res, dim=-1))
         # compare it to the true sentiment - on mismatch ignore, on correct prediction save
-        # ignore not-accurate-enough predictions, since
         true_sentiment = phrase_sentiments[sentence]
-        if int(round(true_sentiment)) != top_idx or 0.4 < float(res[0, top_idx]) < 0.6:
+        if int(round(true_sentiment)) != top_idx:
             continue
+        elif (0.5 - UNSURE_PREDICTION_DELTA) < float(res[0, top_idx]) < (0.5 + UNSURE_PREDICTION_DELTA):
+            # if the prediction is uncertain, save it separately
+            unsure_pred_indices.append(top_idx)
+            bert_tokens_unsure.append(tokenizer.tokenize(sentence))
+            sst_tokens_unsure.append(tokens)
+            unsure_pred_sentences.append(sentence)
         else:
-            target_indices.append(top_idx)
-            bert_tokens.append(tokenizer.tokenize(sentence))
-            sst_tokens.append(tokens)
-            valid_sentences.append(sentence)
+            # save certain predictions
+            correct_pred_indices.append(top_idx)
+            bert_tokens_correct.append(tokenizer.tokenize(sentence))
+            sst_tokens_correct.append(tokens)
+            correct_pred_sentences.append(sentence)
 
     # dump the tokens and predictions
     with open(OUTPUT_DIR + '/sst_bert_tokens.json', 'w+', encoding='utf-8') as f:
-        f.write(json.dumps({'bert_tokens': bert_tokens, 'sst_tokens': sst_tokens}))
+        f.write(json.dumps({'bert_tokens': bert_tokens_correct, 'sst_tokens': sst_tokens_correct}))
 
     with open(OUTPUT_DIR + '/method_file_dict_custom.json', 'w+', encoding='utf-8') as f:
         f.write(json.dumps(method_file_dict))
 
-    create_gradient_attributions(valid_sentences, target_indices)
-    create_smoothgrad_attributions(valid_sentences, target_indices)
-    create_ig_attributions(valid_sentences, target_indices)
-    create_relprop_attributions(valid_sentences, target_indices)
+    # create attributions for the correctly predicted and certain
+    create_gradient_attributions(correct_pred_sentences, correct_pred_indices)
+    create_smoothgrad_attributions(correct_pred_sentences, correct_pred_indices)
+    create_ig_attributions(correct_pred_sentences, correct_pred_indices)
+    create_relprop_attributions(correct_pred_sentences, correct_pred_indices)
+
+    # create attributions for the correctly predicted but uncertain
+    create_gradient_attributions(correct_pred_sentences, correct_pred_indices, UNSURE_DIR)
+    create_smoothgrad_attributions(correct_pred_sentences, correct_pred_indices, UNSURE_DIR)
+    create_ig_attributions(correct_pred_sentences, correct_pred_indices, UNSURE_DIR)
+    create_relprop_attributions(correct_pred_sentences, correct_pred_indices, UNSURE_DIR)
 
 
 if __name__ == '__main__':
