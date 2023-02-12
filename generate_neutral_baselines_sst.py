27d01cbd43e1bac5c330abe10ea2609be41b7f2c
Vojtěch Bartička <aitakaitov@gmail.com>
2023-02-12 16:55:05 +0100

    Fixed SST baseline generation

--- generate_neutral_baselines_sst.py
+++ generate_neutral_baselines_sst.py
@@ -14,32 +14,23 @@ def main(args: dict):
     model = model.to(device)
     model.eval()
 
-    softmax = torch.nn.Softmax(dim=1).to(device)
-
     # extract padding token
     embeddings = model.bert.base_model.embeddings.word_embeddings.weight.data
     embedding_dimensions = embeddings.shape[1]
 
-    padding_embedding = tokenizer.convert_tokens_to_ids('[PAD]')
-    padding_embedding = torch.index_select(embeddings, 0, torch.tensor(padding_embedding).to(device))
-
-
     os.makedirs(args['output_dir'], exist_ok=True)
 
-    for length in [279, 380, 155, 505, 425, 149]:
+    for length in range(args['start'], 129):
         # generate attention mask for the length
         arr = [1 for _ in range(length)]
-        arr.extend([0 for _ in range(512 - length)])
         attention_mask = torch.tensor([arr]).to(device)
 
-        # generate random embeddings and add padding embeddings
-        rnd = torch.randn((1, length, embedding_dimensions), dtype=torch.float32).to(device)
-        padding = torch.unsqueeze(padding_embedding.repeat((512 - length, 1)), 0).to(device)
-        baseline = torch.cat((rnd, padding), 1).to(device).requires_grad_(True)
+        # generate random embeddings
+        baseline = torch.randn((1, length, embedding_dimensions), dtype=torch.float32).to(device).requires_grad_(True)
 
         # use gradients to modify the input embeddings
         lr = args['lr']
-        output = softmax(model(baseline, attention_mask=attention_mask))[:, 0]
+        output = model(baseline, attention_mask=attention_mask)[:, 0]
         res = float(output)
         while abs(res - 0.5) > args['tolerance']:
             grads = torch.autograd.grad(output, baseline)[0]
@@ -48,10 +39,11 @@ def main(args: dict):
             else:
                 baseline = -1 * lr * grads + baseline
 
-            output = softmax(model(baseline, attention_mask=attention_mask))[:, 0]
+            output = model(baseline, attention_mask=attention_mask)[:, 0]
             res = float(output)
             print(res)
 
+        print('save ' + str(length))
         torch.save(baseline, args['output_dir'] + '/' + f'{length}.pt')
 
 
